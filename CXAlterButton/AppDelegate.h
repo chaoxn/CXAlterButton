50624d4302aa898174feb0a753838661c5d35a93
chaoxn <chaoxn@gmail.com>
2015-11-04 14:49:37 +0800

    Initial Commit

---
+++ CXAlterButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXAlterButton
+//
+//  Created by fizz on 15/11/4.
+//  Copyright © 2015年 chaox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
